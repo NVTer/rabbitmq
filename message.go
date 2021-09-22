fe69ca7caa2b4840819f203db9eceb0b20c79141
Terenya Vladimir <vladimir.terenya@gmail.com>
2021-09-22 19:52:11 +0300

    feat: getters for message

--- message.go
+++ message.go
@@ -1,5 +1,17 @@
 package RabbitMQ
 
 func NewMessage(text, ID string, body []byte) *Message {
-	return &Message{text: text,id: ID,body: body}
+	return &Message{text: text, id: ID, body: body}
+}
+
+func (m *Message) GetText() string {
+	return m.text
+}
+
+func (m *Message) GetBody() []byte {
+	return m.body
+}
+
+func (m *Message) GetID() string {
+	return m.id
 }
