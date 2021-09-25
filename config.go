f66e4de7e695ddceca70773ea801d23cf30b2441
Igor Nadenenko <goganad99@gmail.com>
2021-09-25 10:15:10 +0300

    fix: constructors for config

--- config.go
+++ config.go
@@ -6,11 +6,19 @@ type Config struct {
 	Publish  `mapstructure:"publish"`
 }
 
+func NewConfig(queue Queue, channel Channel, publish Publish) *Config {
+	return &Config{Queue: queue, Channel: channel, Publish: publish}
+}
+
 type Queue struct {
 	Durable   bool `mapstructure:"durable"`
 	AutoDel   bool `mapstructure:"auto_delete"`
 	Exclusive bool `mapstructure:"exclusive"`
 	NoWait    bool `mapstructure:"no_wait"`
+}
+
+func NewQueue(durable bool, autoDel bool, exclusive bool, noWait bool) *Queue {
+	return &Queue{Durable: durable, AutoDel: autoDel, Exclusive: exclusive, NoWait: noWait}
 }
 
 type Channel struct {
@@ -20,7 +28,15 @@ type Channel struct {
 	NoWait    bool `mapstructure:"no_wait"`
 }
 
+func NewChannel(autoAck bool, exclusive bool, noLocal bool, noWait bool) *Channel {
+	return &Channel{AutoAck: autoAck, Exclusive: exclusive, NoLocal: noLocal, NoWait: noWait}
+}
+
 type Publish struct {
 	Mandatory   bool `mapstructure:"mandatory"`
 	Immediate   bool `mapstructure:"immediate"`
+}
+
+func NewPublish(mandatory bool, immediate bool) *Publish {
+	return &Publish{Mandatory: mandatory, Immediate: immediate}
 }
